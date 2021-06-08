302d246985bc72cd4cd9dfb080040008fdf811ac
SeongBong <chopisal@gmail.com>
2021-06-08 09:27:39 +0900

    owner

--- src/pages/owner/my-restaurant.tsx
+++ src/pages/owner/my-restaurant.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+import { gql, useMutation, useQuery, useSubscription } from "@apollo/client";
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet-async";
+import { Link, useHistory, useParams } from "react-router-dom";
+import { Dish } from "../../components/dish";
+import { useMe } from "../../hooks/useMe";
+
+interface IParams {
+  id: string;
+}
 
 export const MyRestaurant = () => {
+  const { id } = useParams<IParams>();
+
   return <div>MyRestaurant</div>;
 };
